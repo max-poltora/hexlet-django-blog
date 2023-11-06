d44c99655f4ef29dfa8408e06005668052f21800
Maksim Poltoradnev <kim1.5@mail.ru>
2023-11-06 18:55:29 +0400

    lesson 5

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('Article')
+    return render(request, 'index.html', context = {
+        'app_name': 'hexlet-django-blog',
+        })
 
