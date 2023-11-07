b2c2be39143fced72b32f281dae8e7b5ee0aa4ad
Maksim Poltoradnev <kim1.5@mail.ru>
2023-11-07 14:59:32 +0400

    lesson 6

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
+'''
 def index(request):
-    return render(request, 'index.html', context = {
+    return render(request, 'article/index.html', context = {
         'app_name': 'hexlet-django-blog',
         })
+'''
 
+class IndexView(TemplateView):
+    
+    template_name = 'article/index.html'
+    
+    def get_context_data(self, **kwargs):
+        
+        context = super().get_context_data(**kwargs)
+        context['app_name'] = 'hexlet-django-blog'
+        
+        return context
