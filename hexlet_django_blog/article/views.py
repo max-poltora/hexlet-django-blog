5695e763af3dbd9d426a0875e33aaa73cb2a8645
Maksim Poltoradnev <kim1.5@mail.ru>
2023-11-10 13:53:04 +0400

    lesson 13

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+
 from django.views.generic.base import TemplateView
+from django.views import View
 from .models import Article
 
 '''
@@ -31,3 +32,12 @@ def index(request, tags, article_id):
                       'tags': tags,
                       'article_id': article_id,
                       })
+
+class ArticleView(View):
+    
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id = kwargs['id'])
+        return render(request, 'article/show.html', context = {
+                'article': article,
+            })
+    
